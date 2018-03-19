ada5b44e06e4605c329f2d15b623b93cf15e3989
Brett Case <brett.case97@gmail.com>
2018-03-18 19:55:26 -0700

    adding test files

--- projects/casebr/FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
+++ projects/casebr/FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
@@ -23,26 +23,69 @@ public class UrlValidatorTest extends TestCase {
    public void testManualTest()
    {
 //You can use this function to implement your manual testing	   
-	   
+	   UrlValidator urlV = new UrlValidator();
+     assertFalse(urlV.isValid(null));
+     assertFalse(urlV.isValid("google"));
+     assertFalse(urlV.isValid("h3t://www.facebook.com"));
+     assertTrue(urlV.isValid("http://www.google.com"));
+     assertTrue(urlV.isValid("http://www.google.com/test"));
+      assertTrue(urlV.isValid("http://www.google.com:80/test"));
+     assertFalse(urlV.isValid("http//www.google.com"));
+     assertFalse(urlV.isValid(""));
    }
    
    
    public void testYourFirstPartition()
    {
-	 //You can use this function to implement your First Partition testing	   
-
+	  UrlValidator urlV = new UrlValidator();
+    String[] ports = {":0", ":1", ":65535", ":65536", ":-1"};
+    
+    for (int i = 0; i < ports.length; i++) {
+      if (i > 2){
+        // assertFalse(urlV.isValid("http://www.google.com" + ports[i]));
+      }else{
+        // assertTrue(urlV.isValid("http://www.google.com" + ports[i]));
+      }
+    }
+     
    }
-   
+  
    public void testYourSecondPartition(){
-		 //You can use this function to implement your Second Partition testing	   
-
+     UrlValidator urlV = new UrlValidator();
+     String[] schemes = {"http://", "http:/", "://", ""};
+     
+     for (int i = 0; i < schemes.length; i++) {
+       if (i > 0){
+         assertFalse(urlV.isValid(schemes[i] + "www.google.com"));
+       }else{
+         assertTrue(urlV.isValid(schemes[i] + "www.google.com"));
+       }
+     }
    }
    //You need to create more test cases for your Partitions if you need to 
    
    public void testIsValid()
    {
+     UrlValidator urlV = new UrlValidator();
 	   //You can use this function for programming based testing
-
+     String[] validSchemes = {"http://"};
+     String[] validAuthorities = {"www.google.com", "google.com"};
+     String[] validPorts = {":0", ":65535", ":1", ""};
+     String[] validPaths = {"/test", ""};
+     String[] validQueries = {"", "?action=view"};
+     for(String s : validSchemes){
+       for(String a : validAuthorities){
+         for(String po : validPorts){
+           for(String pa : validPaths){
+             for(String q : validQueries){
+               System.out.println(s + a+ po + pa + q);
+               assertTrue(urlV.isValid(s + a + po + pa + q));
+             }
+           }
+         }
+       }
+     }
+     
    }
    
 
